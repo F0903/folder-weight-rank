eb25064e1278c70951e0108b0dca9a90230b1c08
Frederik P <frepbh@gmail.com>
2022-10-13 06:06:23 +0200

    should now spawn a limited number of threads

--- src/run_args.rs
+++ src/run_args.rs
@@ -1,6 +1,7 @@
 use std::{
     env::Args,
     path::{Path, PathBuf},
+    thread::available_parallelism,
 };
 
 use crate::file_size::SizeUnit;
@@ -11,10 +12,11 @@ pub struct RunArgs {
     path: PathBuf,
     timed: bool,
     size_unit: SizeUnit,
+    thread_count: u8,
 }
 
 impl RunArgs {
-    fn with_arg(&mut self, arg: &str) -> Result<()> {
+    fn with_arg(&mut self, arg: &str, next_arg: Option<&String>) -> Result<()> {
         if !arg.starts_with("-") {
             self.path.push(arg);
             if !self.path.is_dir() {
@@ -24,6 +26,12 @@ impl RunArgs {
         }
         match &arg[1..] {
             "t" => self.timed = true,
+            "tc" => {
+                self.thread_count = match next_arg {
+                    Some(x) => x.parse::<u8>().map_err(|e| e.to_string())?,
+                    None => return Err("Please specify a number to use the 'tc' option.".into()),
+                }
+            }
             "b" | "B" => self.size_unit = SizeUnit::Byte,
             "kb" | "KB" => self.size_unit = SizeUnit::KiloByte,
             "mb" | "MB" => self.size_unit = SizeUnit::MegaByte,
@@ -35,8 +43,14 @@ impl RunArgs {
 
     pub fn from_args(args: Args) -> Result<Self> {
         let mut run_args = Self::default();
-        for arg in args {
-            run_args.with_arg(&arg)?;
+        let mut arg_iter = args.peekable();
+        loop {
+            let arg = match arg_iter.next() {
+                Some(x) => x,
+                None => break,
+            };
+            let next_arg = arg_iter.peek();
+            run_args.with_arg(&arg, next_arg)?;
         }
         Ok(run_args)
     }
@@ -52,6 +66,10 @@ impl RunArgs {
     pub fn get_size_unit(&self) -> SizeUnit {
         self.size_unit
     }
+
+    pub fn get_thread_count(&self) -> u8 {
+        self.thread_count
+    }
 }
 
 impl Default for RunArgs {
@@ -60,6 +78,10 @@ impl Default for RunArgs {
             path: PathBuf::new(),
             timed: false,
             size_unit: SizeUnit::KiloByte,
+            thread_count: available_parallelism()
+                .map(|x| x.get())
+                .unwrap_or(8)
+                .clamp(1, u8::MAX as usize) as u8,
         }
     }
 }
--- src/search.rs
+++ src/search.rs
@@ -1,12 +1,9 @@
-use std::fs::ReadDir;
-
-use {
-    crate::{file_size::FileSize, run_args::RunArgs},
-    std::{
-        fs::{self, read_dir, DirEntry},
-        io::ErrorKind,
-        time, vec,
-    },
+use crate::{file_size::FileSize, run_args::RunArgs};
+use std::{
+    fs::{self, read_dir, DirEntry, ReadDir},
+    io::ErrorKind,
+    sync::atomic::{AtomicU8, Ordering},
+    thread, time, vec,
 };
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
@@ -39,11 +36,22 @@ fn convert_dir_entry(entry: DirEntry) -> Result<Entry> {
     })
 }
 
-fn get_entries(dir: ReadDir) -> Vec<Entry> {
-    //TODO: Only spawn a limited amount of threads.
-    std::thread::scope(|s| {
-        let mut handles = vec![];
+fn get_entries(dir: ReadDir, max_threads: u8) -> Vec<Entry> {
+    let running_threads = AtomicU8::new(0);
+    thread::scope(|s| {
+        let mut handles: Vec<thread::ScopedJoinHandle<Option<Entry>>> = vec![];
         for entry in dir {
+            if running_threads.load(Ordering::Relaxed) >= max_threads {
+                'handle_loop: loop {
+                    for handle in handles.iter() {
+                        if handle.is_finished() {
+                            running_threads.fetch_sub(1, Ordering::Relaxed);
+                            break 'handle_loop;
+                        }
+                        thread::yield_now();
+                    }
+                }
+            }
             let handle = s.spawn(|| {
                 let entry = match entry {
                     Ok(x) => x,
@@ -56,6 +64,7 @@ fn get_entries(dir: ReadDir) -> Vec<Entry> {
                 Some(ent)
             });
             handles.push(handle);
+            running_threads.fetch_add(1, Ordering::Relaxed);
         }
         let mut entries = vec![];
         for handle in handles {
@@ -86,7 +95,7 @@ pub fn search(args: RunArgs) -> Result<()> {
         },
     };
 
-    let mut entries = get_entries(read_dir);
+    let mut entries = get_entries(read_dir, args.get_thread_count());
     entries.sort_unstable_by_key(|x| x.size.get() as usize);
     entries.reverse();
     for entry in entries {
