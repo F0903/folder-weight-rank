1310bcc4b0b27cae173c7395e9344ca96a30b63d
Frederik P <frepbh@gmail.com>
2022-10-14 01:11:09 +0200

    Small refactor on the multithreaded side of things.

--- src/search.rs
+++ src/search.rs
@@ -2,8 +2,12 @@ use crate::{file_size::FileSize, run_args::RunArgs};
 use std::{
     fs::{self, read_dir, DirEntry, ReadDir},
     io::ErrorKind,
-    sync::atomic::{AtomicU8, Ordering},
-    thread, time, vec,
+    sync::{
+        atomic::{AtomicU8, Ordering},
+        mpsc::channel,
+        Arc, Condvar, Mutex,
+    },
+    thread, time,
 };
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
@@ -12,6 +16,9 @@ struct Entry {
     name: String,
     size: FileSize,
 }
+
+unsafe impl Sync for Entry {}
+unsafe impl Send for Entry {}
 
 fn entry_size(entry: &DirEntry) -> Result<u64> {
     let info = entry.metadata()?;
@@ -38,42 +45,41 @@ fn convert_dir_entry(entry: DirEntry) -> Result<Entry> {
 
 fn get_entries(dir: ReadDir, max_threads: u8) -> Vec<Entry> {
     let running_threads = AtomicU8::new(0);
+    let (tx, rx) = channel();
     thread::scope(|s| {
-        let mut handles: Vec<thread::ScopedJoinHandle<Option<Entry>>> = vec![];
+        let cvar_pair = Arc::new((Mutex::new(false), Condvar::new()));
+        let (lock, cvar) = &*cvar_pair;
         for entry in dir {
-            if running_threads.load(Ordering::Relaxed) >= max_threads {
-                'handle_loop: loop {
-                    for handle in handles.iter() {
-                        if handle.is_finished() {
-                            running_threads.fetch_sub(1, Ordering::Relaxed);
-                            break 'handle_loop;
-                        }
-                        thread::yield_now();
-                    }
-                }
+            if running_threads.load(Ordering::Acquire) >= max_threads {
+                let mut all_busy = lock.lock().unwrap();
+                *all_busy = true;
+                let _ = cvar.wait(all_busy).unwrap();
             }
-            let handle = s.spawn(|| {
+            let cvar_pair = cvar_pair.clone();
+            let tx = tx.clone();
+            s.spawn(|| {
+                let cvar_pair = cvar_pair;
+                let tx = tx;
                 let entry = match entry {
                     Ok(x) => x,
-                    Err(_) => return None,
+                    Err(_) => return,
                 };
                 let ent = match convert_dir_entry(entry) {
                     Ok(x) => x,
-                    Err(_) => return None,
+                    Err(_) => return,
                 };
-                Some(ent)
+                running_threads.fetch_sub(1, Ordering::AcqRel);
+                let (lock, cvar) = &*cvar_pair;
+                let mut all_busy = lock.lock().unwrap();
+                *all_busy = false;
+                cvar.notify_one();
+                tx.send(ent).unwrap();
             });
-            handles.push(handle);
-            running_threads.fetch_add(1, Ordering::Relaxed);
+            running_threads.fetch_add(1, Ordering::Release);
         }
-        let mut entries = vec![];
-        for handle in handles {
-            if let Some(x) = handle.join().unwrap() {
-                entries.push(x);
-            }
-        }
-        entries
-    })
+    });
+    drop(tx); // Must be manually dropped or else the statement below will never return.
+    rx.iter().collect()
 }
 
 pub fn search(args: RunArgs) -> Result<()> {
